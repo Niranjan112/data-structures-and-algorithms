96434abbc83d89ca99cc602587dc87d8732254b0
Niranjan Sharma <niranjan.sharma@mercedes-benz.com>
2025-08-08 19:49:46 +0530

    print all divisor problem added

---
+++ take_u_forward/b_basic_maths/f_print_all_divisors/PrintAllDivisors.java
@@ -0,0 +1,46 @@
+package take_u_forward.b_basic_maths.f_print_all_divisors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintAllDivisors {
+
+  public static void main(String[] args) {
+    int n = 36;
+    System.out.println("Divisors of " + n + ": " + bruteForce(n));
+    System.out.println("Divisors of " + n + " (Optimal): " + optimalApproach(n));
+  }
+
+  public static List<Integer> bruteForce(int n) {
+
+    ArrayList<Integer> divisors = new ArrayList<>();
+    for (int i = 1; i <= n; i++) {
+      if(n % i == 0) {
+        divisors.add(i);
+      }
+    }
+
+    return divisors;
+  }
+
+  public static List<Integer> optimalApproach(int n) {
+    /*
+    *
+    * */
+
+    int sqrtN = (int) Math.sqrt(n);
+
+    ArrayList<Integer> divisors = new ArrayList<>();
+    for (int i = 1; i <= sqrtN; i++) {
+      if(n % i == 0) {
+        divisors.add(i);
+
+        if(i != sqrtN) {
+          divisors.add(n / i);
+        }
+      }
+    }
+
+    return divisors;
+  }
+}
