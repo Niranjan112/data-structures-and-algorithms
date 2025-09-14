86caff99a167a7de4d797f65d5e7c1153964a785
Niranjan Sharma <niranjan.sharma@mercedes-benz.com>
2025-09-15 02:48:34 +0530

    problem added

--- take_u_forward/f_array_problems/a_easy/K_FindMissingNumInArr.java
+++ take_u_forward/f_array_problems/a_easy/K_FindMissingNumInArr.java
@@ -11,9 +11,9 @@ public class K_FindMissingNumInArr {
 
     int[] arr = {9,6,4,2,3,5,7,0,1};
 
-    System.out.println("Missing Number: " + bruteForceApproach(arr));
-    System.out.println("Missing Number: " + betterApproach(arr));
-    System.out.println("Missing Number: " + optimalApproach(arr));
+    System.out.println("Missing Number: " + bruteForceApproach(arr)); // O(n2) time complexity and O(1) space complexity
+    System.out.println("Missing Number: " + betterApproach(arr)); // O(n + n) = O(2n) time complexity and O(n) space complexity
+    System.out.println("Missing Number: " + optimalApproach(arr)); // O(n) time complexity and O(1) space complexity
   }
 
   public static int bruteForceApproach(int[] arr) {
